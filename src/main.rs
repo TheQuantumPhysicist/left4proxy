778a44f871694de7ebd7965b5cc7f444667cbe79
Samer Afach <info@afach.de>
2023-01-03 22:00:42 +0400

    Use io::copy

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,61 @@
 use async_std::net::{TcpListener, TcpStream};
-use async_std::prelude::*;
 use async_std::task;
+use async_std::{io, prelude::*};
 
 const POSSIBLE_DESTINATIONS: [&str; 3] = ["127.0.0.1:55880", "10.10.0.11:55880", "127.0.0.1:8880"];
 
-async fn from_a_to_b(a: &mut TcpStream, b: &mut TcpStream) {
-    let mut buf = Vec::new();
-    match a.read_to_end(&mut buf).await {
-        Ok(_) => (),
-        Err(e) => {
-            eprintln!("Buffer incoming read error: {e}");
-            return;
-        }
-    }
+// async fn from_a_to_b(a: &mut TcpStream, b: &mut TcpStream) {
+//     let mut buf = Vec::new();
+//     match a.read_to_end(&mut buf).await {
+//         Ok(_) => (),
+//         Err(e) => {
+//             eprintln!("Buffer incoming read error: {e}");
+//             return;
+//         }
+//     }
 
-    if buf.is_empty() {
-        return;
-    }
+//     if buf.is_empty() {
+//         return;
+//     }
 
-    println!("Read {} bytes from a", buf.len());
+//     println!("Read {} bytes from a", buf.len());
 
-    match b.write_all(&buf).await {
-        Ok(_) => (),
-        Err(e) => {
-            eprintln!("Buffer outgoing write error: {e}");
-            return;
-        }
-    }
+//     match b.write_all(&buf).await {
+//         Ok(_) => (),
+//         Err(e) => {
+//             eprintln!("Buffer outgoing write error: {e}");
+//             return;
+//         }
+//     }
 
-    println!("Wrote {} bytes to b", buf.len());
-}
+//     println!("Wrote {} bytes to b", buf.len());
+// }
 
 async fn do_tunnel(mut incoming: TcpStream, mut outgoing: TcpStream) {
-    loop {
-        from_a_to_b(&mut incoming,&mut  outgoing).await;
-        from_a_to_b(&mut outgoing,&mut  incoming).await;
-    }
+    io::copy(&mut incoming, &mut outgoing)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("Copy from incoming to outgoing failed {e}");
+            0
+        });
+    io::copy(&mut outgoing, &mut incoming)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("Copy from outgoing to incoming failed {e}");
+            0
+        });
+    io::copy(&mut incoming, &mut outgoing)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("Copy from incoming to outgoing failed {e}");
+            0
+        });
+    io::copy(&mut outgoing, &mut incoming)
+        .await
+        .unwrap_or_else(|e| {
+            eprintln!("Copy from outgoing to incoming failed {e}");
+            0
+        });
 }
 
 async fn handle_connection(incoming_stream: TcpStream) {
