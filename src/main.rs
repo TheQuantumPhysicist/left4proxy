ba2d19770bb87f73fad696d213c0b10b36796b3b
Samer Afach <info@afach.de>
2023-01-03 22:02:46 +0400

    Remove redundancy in copying

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,10 @@
+use async_std::io;
 use async_std::net::{TcpListener, TcpStream};
 use async_std::task;
-use async_std::{io, prelude::*};
 
 const POSSIBLE_DESTINATIONS: [&str; 3] = ["127.0.0.1:55880", "10.10.0.11:55880", "127.0.0.1:8880"];
 
-// async fn from_a_to_b(a: &mut TcpStream, b: &mut TcpStream) {
-//     let mut buf = Vec::new();
-//     match a.read_to_end(&mut buf).await {
-//         Ok(_) => (),
-//         Err(e) => {
-//             eprintln!("Buffer incoming read error: {e}");
-//             return;
-//         }
-//     }
-
-//     if buf.is_empty() {
-//         return;
-//     }
-
-//     println!("Read {} bytes from a", buf.len());
-
-//     match b.write_all(&buf).await {
-//         Ok(_) => (),
-//         Err(e) => {
-//             eprintln!("Buffer outgoing write error: {e}");
-//             return;
-//         }
-//     }
-
-//     println!("Wrote {} bytes to b", buf.len());
-// }
-
 async fn do_tunnel(mut incoming: TcpStream, mut outgoing: TcpStream) {
-    io::copy(&mut incoming, &mut outgoing)
-        .await
-        .unwrap_or_else(|e| {
-            eprintln!("Copy from incoming to outgoing failed {e}");
-            0
-        });
-    io::copy(&mut outgoing, &mut incoming)
-        .await
-        .unwrap_or_else(|e| {
-            eprintln!("Copy from outgoing to incoming failed {e}");
-            0
-        });
     io::copy(&mut incoming, &mut outgoing)
         .await
         .unwrap_or_else(|e| {
